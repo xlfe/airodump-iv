9662c07fa0f1df091cc51dbc07ff1ec6313acf3a
Felix Barbalet <felix@pv.tl>
2016-09-14 22:14:47 +1000

    try 5 min intervals

--- airodump-log.py
+++ airodump-log.py
@@ -3,8 +3,9 @@
 """
 
 import os
-from datetime import datetime
+from datetime import datetime, timedelta
 import json
+import collections
 from optparse import OptionParser
 from random import randint
 from scapy.all import sniff
@@ -16,20 +17,105 @@ from scapy.layers.dot11 import Dot11ProbeResp
 from scapy.layers.dot11 import RadioTap
 import scapy_ex
 import logging
+from threading import Timer
+
 
 from we import WirelessExtension
 
 BROADCAST_BSSID = 'ff:ff:ff:ff:ff:ff'
 
 log = logging.getLogger('airodump')
-log.setLevel(logging.DEBUG)
-
-formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-
+log.setLevel(logging.INFO)
 sh = logging.StreamHandler()
-sh.setFormatter(formatter)
 log.addHandler(sh)
 
+Event = collections.namedtuple('event', field_names=['timestamp','power'])
+
+class Observations(object):
+    """
+    Track observations
+    Emit a event at least every TIME_PERIOD
+    And if they deviate from 90% of their TIME_PERIOD average, emit an event straight away
+    """
+    TIME_PERIOD = 300
+
+    KEYS = [
+        'type',
+        'mac',
+        'ssid'
+    ]
+
+    TRACK = [
+        'power'
+    ]
+
+    def __init__(self, details):
+        self.details = details
+        self.events = []
+        self.timeout = None
+        self.LAST_EMIT = None
+
+    @classmethod
+    def sig(cls, details):
+        return '-'.join(details[k] for k in cls.KEYS)
+
+    def emit(self):
+        self.LAST_EMIT = datetime.now()
+        d = self.details.copy()
+        d['avg_power'] = '{:.2f}'.format(self.p_avg)
+        d['count'] = len(self.events)
+        print json.dumps(d, sort_keys=True)
+
+    @property
+    def p_avg(self):
+
+        p_sum = sum(e.power for e in self.events)
+        try:
+            p_avg = (p_sum*1.0) / len(self.events)
+        except ZeroDivisionError:
+            p_avg = 0
+        return p_avg
+
+    def expire(self):
+
+        self.purge()
+
+        now = datetime.now()
+        if self.LAST_EMIT is None:
+            self.emit()
+            return
+        if (now - self.LAST_EMIT).total_seconds() > self.TIME_PERIOD:
+            self.emit()
+
+    def reset(self):
+
+        if self.timeout:
+            self.timeout.cancel()
+        self.timeout = Timer(self.TIME_PERIOD, self.expire)
+        self.timeout.start()
+
+    def event(self, d):
+        #remove old events
+        self.purge()
+
+        power = abs(int(d['power']))
+        self.details['power'] = d['power']
+
+        p_avg = self.p_avg
+        diff = abs(p_avg - power)
+
+        if diff > p_avg * 0.15:
+            self.emit()
+
+        self.events.append(Event(datetime.now(), power))
+        self.reset()
+
+    def purge(self):
+        cutoff = datetime.now() - timedelta(seconds=self.TIME_PERIOD)
+        self.events = filter(lambda e: e.timestamp > cutoff, self.events)
+
+
+log_keeper = {}
 
 
 class OUILookup(object):
@@ -111,7 +197,7 @@ class Dot11Scanner:
         if packet.haslayer(Dot11Beacon):
             #Beacon from Access Point
 
-            assert bssid != BROADCAST_BSSID
+            # assert bssid != BROADCAST_BSSID
 
             details = dict(
                 type = 'ap_beacon',
@@ -124,7 +210,6 @@ class Dot11Scanner:
 
         elif packet.haslayer(Dot11ProbeReq):
             # Station looking for an Access Point
-            assert bssid == BROADCAST_BSSID
 
             sta_bssid = packet[Dot11].sta_bssid()
 
@@ -139,37 +224,33 @@ class Dot11Scanner:
 
         else:
             return False
-            details = dict(
-                unknown=packet.__class__.__name__
-            )
+        #     details = dict(
+        #         unknown=packet.__class__.__name__
+        #     )
 
-        log.debug(json.dumps(details, sort_keys=True))
+        key = Observations.sig(details)
+        if key not in log_keeper:
+            log_keeper[key] = Observations(details)
+        log_keeper[key].event(details)
+        # log.debug(json.dumps(details, sort_keys=True))
 
         return False
 
 
     def scan(self, window=None):
 
-        if self.scanner_options.channel_hop:
+        while True:
             self.scanner_options.channel = randint(1, self.scanner_options.max_channel)
             self.set_channel(self.scanner_options.channel)
-        elif -1 != self.scanner_options.channel:
-            self.set_channel(self.scanner_options.channel)
 
-        while True:
             sniff(
                     iface=self.scanner_options.iface,
                     store=False,
                     count=0,
                     offline=None,
-                    timeout=3,
+                    timeout=1,
                     lfilter=self.parse_packet
             )
-
-            if self.scanner_options.channel_hop:
-                self.scanner_options.channel = ((
-                                                self.scanner_options.channel + 3) % self.scanner_options.max_channel) + 1
-                self.set_channel(self.scanner_options.channel)
 
 
     def set_channel(self, channel):
